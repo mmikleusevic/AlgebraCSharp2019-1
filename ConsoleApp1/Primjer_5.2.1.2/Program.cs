7f8e65d5cb197157dc5e3afa87b2fc45e4b988e9
mikleusevicmarin <mikleusevicmarin@gmail.com>
2019-02-25 16:49:21 +0100

    dodan primjer 5.2.1.2

---
+++ ConsoleApp1/Primjer_5.2.1.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primjer_5._2._1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite prirodan broj: ");
+            int a = int.Parse(Console.ReadLine());
+            if (a > 0)
+            {
+                Console.WriteLine("Uneseni broj je pozitivan");
+            }
+            else if(a < 0)
+            {
+                Console.WriteLine("Uneseni broj je negativan");
+            }
+            else
+            {
+                Console.WriteLine("Uneseni broj je jednak 0");
+            }
+            Console.ReadKey();
+        }
+    }
+}
