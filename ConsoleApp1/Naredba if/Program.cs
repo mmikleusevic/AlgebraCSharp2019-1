fd2a7633c8350d8282593af23938ebea9ce20fe0
mikleusevicmarin <mikleusevicmarin@gmail.com>
2019-02-25 17:31:36 +0100

    dodan exception

--- ConsoleApp1/Naredba if/Program.cs
+++ ConsoleApp1/Naredba if/Program.cs
@@ -10,17 +10,25 @@ namespace Naredba_if
     {
         static void Main(string[] args)
         {
-            Console.Write("Unesite prirodan broj: ");
-            int a = int.Parse(Console.ReadLine());
-            if (a > 10)
+            try
             {
-                Console.WriteLine("Uneseni broj je veći od 10.");
+                Console.Write("Unesite prirodan broj: ");
+                int a = int.Parse(Console.ReadLine());
+
+                if (a > 10)
+                {
+                    Console.WriteLine("Uneseni broj je veći od 10.");
+                }
+                else
+                {
+                    Console.WriteLine("Uneseni broj NIJE veći od 10.");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                Console.WriteLine("Uneseni broj NIJE veći od 10.");
+                Console.WriteLine("Opis greške:" + ex.Message);
             }
             Console.ReadKey();
+            }
         }
     }
-}
---
+++ ConsoleApp1/Naredba switch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naredba_switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 0, b = 0;
+            string operacija = "";
+
+            Console.Write("Unesite 1. broj: ");
+            a = float.Parse(Console.ReadLine());
+            Console.Write("Unesite 2. broj: ");
+            b = float.Parse(Console.ReadLine());
+            Console.Write("Unesite računsku operaciju (+,-,*,/): ");
+            operacija = Console.ReadLine();
+
+            switch (operacija)
+            {
+                case "+":
+                    Console.WriteLine("Zbroj: {0}", a + b);
+                    break;
+                case "-":
+                    Console.WriteLine("Razlika: {0}", a - b);
+                    break;
+                case "*":
+                    Console.WriteLine("Umnožak: {0}", a * b);
+                    break;
+                case "/":
+                    Console.WriteLine("Kvocijent: {0}", a / b);
+                    break;
+                default:
+                    Console.WriteLine("Nepoznata računska operacija!");
+                    break;
+            }
+            Console.WriteLine("Pritisnite neku tipku za kraj...");
+            Console.ReadKey();
+        }
+    }
+}
