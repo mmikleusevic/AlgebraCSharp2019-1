a4308b29c848d56af8b894d23d5aee6a390a8b39
mikleusevicmarin <mikleusevicmarin@gmail.com>
2019-02-26 16:16:29 +0100

    dodani zadaci 5.2.3, 5.2.4

---
+++ ConsoleApp1/zadatak_5.2.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj: ");
+
+            int a = int.Parse(Console.ReadLine());
+
+            if (a >= 100)
+            {
+                Console.WriteLine("Broj je velik.");
+            }
+            else
+            {
+                Console.WriteLine("Broj NIJE velik.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/zadatak_5.2.4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj: ");
+
+            int a = int.Parse(Console.ReadLine());
+
+            if (a < 0) a = -a;
+
+            if (a < 10)
+            {
+                Console.WriteLine("Unešeni broj je jednoznamenkast.");
+            }
+            else if(a < 100)
+            {
+                Console.WriteLine("Unešeni broj je dvoznamenkast.");
+            }
+            else if(a < 1000)
+            {
+                Console.WriteLine("Unešeni broj je troznamenkast.");
+            }
+            else
+            {
+                Console.WriteLine("Unešeni broj je višeznamenkast.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
