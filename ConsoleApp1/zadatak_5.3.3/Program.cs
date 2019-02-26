687fe1cbf34d92cd772fe13051cddbc59b881b27
mikleusevicmarin <mikleusevicmarin@gmail.com>
2019-02-26 18:36:36 +0100

    dodan zadatak 5.3.3

---
+++ ConsoleApp1/zadatak_5.3.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Suma prvih sto
+            int suma = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                suma = suma + i;
+            }
+            Console.Write("Zbroj prvih sto brojeva je {0}", suma);
+            Console.ReadLine();
+        }
+    }
+}
