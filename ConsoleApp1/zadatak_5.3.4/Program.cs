512646d0d1b3e3c7a6c191ac90a80a317e39a197
mikleusevicmarin <mikleusevicmarin@gmail.com>
2019-02-27 16:57:56 +0100

    dodan zadatak 5.3.5

--- ConsoleApp1/zadatak_5.3.4/Program.cs
+++ ConsoleApp1/zadatak_5.3.4/Program.cs
@@ -14,6 +14,14 @@ namespace zadatak_5._3._4
             try
             {
                 int a = int.Parse(Console.ReadLine());
+                if (a <= 0)
+                {
+                    throw new NegativeNumberException("Negativni broj");
+                }
+                if (a >= 20)
+                {
+                    throw new VeciOdDvajstException("Broj veći od 20");
+                }
                 int fact = 1;
                 for (int i = a; i > 0; i--)
                 {
@@ -21,12 +29,57 @@ namespace zadatak_5._3._4
                 }
                 Console.WriteLine(fact);
             }
-            
+            catch (NegativeNumberException Nex)
+            {
+                Console.WriteLine("Dozvoljeni brojevi su u intervalu [1,19].", Nex.ToString());
+            }
+            catch (VeciOdDvajstException Vex)
+            {
+                Console.WriteLine("Broj je prevelik, veći je od dimenzije int", Vex.ToString());
+            }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
-            Console.ReadKey();
+            finally
+            {
+                Console.ReadKey();
+            }
+
+        }
+    }
+    public class NegativeNumberException : Exception
+    {
+        public NegativeNumberException()
+        {
+
+        }
+        public NegativeNumberException(string message)
+            : base(message)
+        {
+            Console.WriteLine("Dodatno custom pojašnjenje klase NegativeNumberException");
+        }
+        public NegativeNumberException(string message, Exception inner)
+            : base(message, inner)
+        {
+
+        }
+    }
+    public class VeciOdDvajstException : Exception
+    {
+        public VeciOdDvajstException()
+        {
+
+        }
+        public VeciOdDvajstException(string message)
+            : base(message)
+        {
+
+        }
+        public VeciOdDvajstException(string message, Exception inner)
+            : base(message, inner)
+        {
+
         }
     }
 }
