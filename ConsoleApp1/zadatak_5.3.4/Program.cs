23d4b6b9702bae4c334d30288902a2e91b6e2c93
mikleusevicmarin <mikleusevicmarin@gmail.com>
2019-02-27 16:17:23 +0100

    dodan zadatak 5.3.4

---
+++ ConsoleApp1/zadatak_5.3.4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite cijeli pozitivan broj");
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+                int fact = 1;
+                for (int i = a; i > 0; i--)
+                {
+                    fact *= i;
+                }
+                Console.WriteLine(fact);
+            }
+            
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
