12c813ebbea4477c5090d0265cbf536bb9d3e14a
mikleusevicmarin <mikleusevicmarin@gmail.com>
2019-03-05 18:32:26 +0100

    dodani potprogrami

---
+++ ConsoleApp1/7.2_potprogrami/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2_potprogrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            HelloWorld();
+            HelloWorld();
+            Console.WriteLine(HelloWorld("Ola"));
+
+            Kraj();
+        }
+        static void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+        static string HelloWorld(string poz)
+        {
+            return poz + ", i tebi oj putniče";
+        }
+        static void Kraj()
+        {
+            Console.WriteLine("Pritisnite neku tipku za kraj...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/primjer_7.2.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primjer_7._2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pozdrav();
+            Pozdrav("Pero");
+            Pozdrav("Pero", "Perić");
+            Console.ReadKey();
+        }
+
+        static void Pozdrav()
+        {
+            Console.WriteLine("Dobar dan");
+        }
+
+        static void Pozdrav(string ime)
+        {
+            Console.WriteLine("Dobar dan, " + ime + "!");
+        }
+
+        static void Pozdrav(string ime, string prezime)
+        {
+            Console.WriteLine("Dobar dan, " + ime + " " + prezime + "!");
+        }
+    }
+}
