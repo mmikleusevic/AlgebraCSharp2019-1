fabe2ecc8d94382c4146646787c4e0394f5c38eb
mikleusevicmarin <mikleusevicmarin@gmail.com>
2019-03-06 16:56:20 +0100

    popravak %

--- ConsoleApp1/7.2.10_ukupna_cijena/Program.cs
+++ ConsoleApp1/7.2.10_ukupna_cijena/Program.cs
@@ -20,7 +20,7 @@ namespace _7._2._10_ukupna_cijena
             int ulazna = 0;
             double marza = 0;
             double porez = 0;
-            double pdv = 25%;
+            double pdv = 0.25;
             Console.WriteLine("odaberite'1 za <100', '2 za 100-250', '3 za 250-500', '4 za 500-1000', '5 za >1000'");
             ulazna = int.Parse(Console.ReadLine());
             switch (ulazna)
@@ -52,7 +52,7 @@ namespace _7._2._10_ukupna_cijena
             }
             double ul;
             ul = uk - marza;
-            Console.WriteLine("porez na ulaznu cijenu je {1}, a pdv na ulaznu cijenu je {2}", ul, porez*100, pdv*100);
+            Console.WriteLine("porez na ulaznu cijenu je {1}%, a pdv na ulaznu cijenu je {2}%", ul, porez*100, pdv*100);
             return ul;
         }
     }
