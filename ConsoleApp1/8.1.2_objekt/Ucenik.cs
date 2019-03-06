9e7f5710647e439591c53c1b06aa2321ff7f2c0e
mikleusevicmarin <mikleusevicmarin@gmail.com>
2019-03-06 18:44:35 +0100

    dodani objekti

---
+++ ConsoleApp1/8.1.2_objekt/Ucenik.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _8._1._2_objekt
+{
+    internal class Ucenik
+    {
+        public DateTime datumNastanka;
+        public string ime = "";
+        public string prezime = "";
+        public int ocjenaIzMatematike = 0;
+        public int ocjenaIzEngleskog = 0;
+        public int ocjenaIzBiologije = 0;
+
+        public Ucenik()
+        {
+            this.datumNastanka = DateTime.Now;
+        }
+
+        public Ucenik(string v)
+        {
+            this.ime = v;
+        }
+        public Ucenik(string v,string prez)
+        {
+            this.ime = v;
+            this.prezime = prez;
+        }
+
+        public double Prosjek()
+        {
+            double prosjek = 0;
+            prosjek = (double)(ocjenaIzMatematike + ocjenaIzEngleskog + ocjenaIzBiologije) / 3;
+            return prosjek;
+        }
+        public override string ToString()
+        {
+            return "Moje ime je " + this.ime + " a moj prosjek je: " + this.Prosjek();
+        }
+    }
+}
