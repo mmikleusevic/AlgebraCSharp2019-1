22b1bad20da5dda6f04e6062139a7a2954bc349e
mikleusevicmarin <mikleusevicmarin@gmail.com>
2019-03-07 23:36:56 +0100

    zadaca dodana

---
+++ ConsoleApp1/8.2.1_static/Static.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8._2._1_static
+{
+    internal class Static
+    {
+        // PRIVATE konstruktor onemogućava instanciranje
+        private Static() { }
+
+        #region STATIC metode
+
+        public static double Zbroj(double broj1, double broj2)
+        {
+            return broj1 + broj2;
+        }
+
+        public static double Kub(double broj)
+        {
+            return broj * broj * broj;
+        }
+
+        public static double UdaljenostTocaka(double x1, double x2, double y1, double y2)
+        {
+            return Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        }
+
+        public static double CelzijFahrenheit(double stupnjevi)
+        {
+            return ((double)9 / 5) * stupnjevi + 32;
+        }
+
+        #endregion
+    }
+}
