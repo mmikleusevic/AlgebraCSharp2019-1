99decbd7773c420347e1a8e7df6dc01d9c50b76e
mikleusevicmarin <mikleusevicmarin@gmail.com>
2019-03-08 18:33:15 +0100

    lambda funkcije

--- ConsoleApp1/8.1.3_zaposlenik/Program.cs
+++ ConsoleApp1/8.1.3_zaposlenik/Program.cs
@@ -10,11 +10,68 @@ namespace _8._1._3_zaposlenik
     {
         static void Main(string[] args)
         {
-
+            // Prvi način
             // Instanciranje novog objekta Z1 klase Zaposlenik
-            Zaposlenik Z1 = new Zaposlenik("Pero","Perić","1234567890123");
+            // Zaposlenik Z1 = new Zaposlenik("Pero","Perić","1234567890123");
+            // Zaposlenik Z2 = new Zaposlenik("Marko", "Marić");
+
+            // List <Zaposlenik> listaZaposlenika  = new List<Zaposlenik>();
+            // listaZaposlenika.Add(Z1);
+            // listaZaposlenika.Add(Z2);
+
+            // Drugi način
+            List<Zaposlenik> listaZaposlenika = new List<Zaposlenik>()
+            {
+                new Zaposlenik("Pero", "Perić", "1234567890123"),
+                new Zaposlenik("Marko", "Marić")
+            };
+
+
+            listaZaposlenika.ForEach(Zaposlenik => UnesiBodove(Zaposlenik));
+            listaZaposlenika.ForEach(Zaposlenik => UnesiVrijednost(Zaposlenik));
+            listaZaposlenika.ForEach(Zaposlenik => IspisiZaposlenike(Zaposlenik));
 
             // Unesi pravilno broj bodova
+
+            // Ispis
+            Console.WriteLine("Zaposlenik: " + Z1.Ime + " " + Z1.Prezime);
+            Console.WriteLine("Neto: " + Z1.NetoIzracunPlace());
+            Console.WriteLine("Porez: " + Z1.Porez);
+            Console.WriteLine("Bruto: " + Z1.BrutoIzracunPlace());
+
+            Console.ReadKey();
+        }
+
+        private static void IspisiZaposlenike(Zaposlenik zaposlenik)
+        {
+            
+        }
+
+        private static void UnesiVrijednost(Zaposlenik zaposlenik)
+        {
+            bool provjeraUnosa = false;
+            while (!provjeraUnosa)
+            {
+                Console.Write("Unesite vrijednost boda: ");
+
+                try
+                {
+                    zaposlenik.VrijednostBoda = double.Parse(Console.ReadLine());
+                    provjeraUnosa = true;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Greška: " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Greška: " + ex.Message);
+                }
+            }
+        }
+
+        private static void UnesiBodove(Zaposlenik zaposlenik)
+        {
             bool provjeraUnosa = false;
             while (!provjeraUnosa)
             {
@@ -22,7 +79,7 @@ namespace _8._1._3_zaposlenik
 
                 try
                 {
-                    Z1.BrojBodova = double.Parse(Console.ReadLine());
+                    zaposlenik.BrojBodova = double.Parse(Console.ReadLine());
                     provjeraUnosa = true;
                 }
                 catch (FormatException ex)
@@ -34,35 +91,6 @@ namespace _8._1._3_zaposlenik
                     Console.WriteLine("Greška: " + ex.Message);
                 }
             }
-
-
-            provjeraUnosa = false;
-            while (!provjeraUnosa)
-            {
-                Console.Write("Unesite vrijednost boda: ");
-
-                try
-                {
-                    Z1.VrijednostBoda = double.Parse(Console.ReadLine());
-                    provjeraUnosa = true;
-                }
-                catch (FormatException ex)
-                {
-                    Console.WriteLine("Greška: " + ex.Message);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Greška: " + ex.Message);
-                }
-            }
-
-            // Ispis
-            Console.WriteLine("Zaposlenik: " + Z1.Ime + "" + Z1.Prezime);
-            Console.WriteLine("Neto: " + Z1.NetoIzracunPlace());
-            Console.WriteLine("Porez: " + Z1.Porez);
-            Console.WriteLine("Bruto: " + Z1.BrutoIzracunPlace());
-
-            Console.ReadKey();
         }
     }
 }
