b8ae9e34c8f88001f403c50aaa22876087511162
mikleusevicmarin <mikleusevicmarin@gmail.com>
2019-03-13 18:22:04 +0100

    stringovi

---
+++ ConsoleApp1/9.1.1_17_manipulacija/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1._1_17_manipulacija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("9.1.1. Znak u rijeci");
+            string rijec = "Danas je suncan dan";
+            char slovo = 'n';
+            int brojac = 0;
+            for (int i = 0; i < rijec.Length; i++)
+            {
+                if(rijec[i] == slovo)
+                {
+                    brojac++;
+                }
+            }
+            Console.WriteLine("Znak {0} se u rijeci {1} pojavljuje {2} puta ",slovo,rijec,brojac);
+            Console.ReadKey();
+        }
+    }
+}
