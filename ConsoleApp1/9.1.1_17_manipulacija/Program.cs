2233fdac077ea1ce056634914a41671f755f3122
mikleusevicmarin <mikleusevicmarin@gmail.com>
2019-03-13 18:50:13 +0100

    dodan zadatak

--- ConsoleApp1/9.1.1_17_manipulacija/Program.cs
+++ ConsoleApp1/9.1.1_17_manipulacija/Program.cs
@@ -10,18 +10,47 @@ namespace _9._1._1_17_manipulacija
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("9.1.1. Znak u rijeci");
-            string rijec = "Danas je suncan dan";
+            string recenica = "Danas je suncan dan, zato vam dobar dan";
+            string rijec = "dan";
             char slovo = 'n';
             int brojac = 0;
-            for (int i = 0; i < rijec.Length; i++)
+            for (int i = 0; i < recenica.Length; i++)
             {
-                if(rijec[i] == slovo)
+                if(recenica[i] == slovo)
                 {
                     brojac++;
                 }
             }
-            Console.WriteLine("Znak {0} se u rijeci {1} pojavljuje {2} puta ",slovo,rijec,brojac);
+            Console.WriteLine("\n9.1.1. Znak u rijeci");
+            Console.WriteLine("Znak {0} se u rijeci '{1}' pojavljuje {2} puta\n ",slovo,recenica,brojac);
+            brojac = 0;
+            string[] nizrijeci = recenica.Split(' ',',','!');
+            // recenica = recenica.ToLower();
+            for (int i = 0; i < nizrijeci.Length; i++)
+            {
+                if(nizrijeci[i] == rijec)
+                {
+                    brojac++;
+                }
+                Console.WriteLine(nizrijeci[i]);
+            }
+
+
+            Console.WriteLine("\n9.1.2. Rijec u recenici");
+            Console.WriteLine("Rijec {0} se u recenici '{1}' pojavljuje {2} puta\n ", rijec, recenica, brojac);
+
+
+            nizrijeci = recenica.Split(' ');
+            brojac = 0;
+            for (int i = 0; i < nizrijeci.Length; i++)
+            {
+                Console.WriteLine(nizrijeci[i]);
+                brojac++;
+            }
+
+            Console.WriteLine("\n9.1.3. Rijec u novi red");
+            Console.WriteLine("Nalazi se {0} rijeci u recenici {1}\n ", brojac, recenica);
+
             Console.ReadKey();
         }
     }
