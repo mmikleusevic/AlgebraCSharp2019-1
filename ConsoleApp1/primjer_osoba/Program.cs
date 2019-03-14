ee5d7be496cb01890d6b4586cecb5df03f7a713a
mikleusevicmarin <mikleusevicmarin@gmail.com>
2019-03-14 18:42:47 +0100

    dodan linq

---
+++ ConsoleApp1/primjer_osoba/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer;
+
+namespace primjer_osoba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ucenik uc1 = new Ucenik("Maja","Majić");
+            Ucenik uc2 = new Ucenik("Ana","Majić");
+            Ucenik uc3 = new Ucenik("Maja","Zmajić");
+            List<Osoba> osobe = new List<Osoba>();
+            List<Osoba> samoMaje = new List<Osoba>();
+            osobe.Add(uc1);
+            osobe.Add(uc2);
+            osobe.Add(uc3);
+
+            string trazenoIme = "Maja";
+            int brojacMaja = 0;
+            foreach (var item in osobe)
+            {
+                if(item.Ime == trazenoIme)
+                {
+                    brojacMaja++;
+                    Console.WriteLine(item.Ime);
+                }  
+            }
+            Console.WriteLine(trazenoIme + " se pojavljuje " + brojacMaja + " puta");
+
+            // from <alias> in <collection>
+            try
+            {
+                    samoMaje = (
+                    from os 
+                    in osobe
+                    where os.Ime == trazenoIme
+                    // select os).SingleOrDefault();
+                    select os).Take(2).ToList();
+
+                // Console.WriteLine("Našao sam osobu " + trazenaOsoba.Ime + " " + trazenaOsoba.prezime);
+            }
+            catch (InvalidOperationException ioe)
+            {
+                Console.WriteLine("Greška: " + ioe.Message);
+            }
+            finally
+            {
+                foreach (var item in samoMaje)
+                {
+                    Console.WriteLine("Moje ime je "+item.Ime+" "+item.prezime);
+                }
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
